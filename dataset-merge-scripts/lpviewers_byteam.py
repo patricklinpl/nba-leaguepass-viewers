100513c30077a14ff0a4e3d00c38b775626a1f97
sevenelevenkevin <kev.zhu@mail.utoronto.ca>
2018-09-16 11:41:59 -0400

    donapoeltl gitting his

--- dataset-merge-scripts/lpviewers_byteam.py
+++ dataset-merge-scripts/lpviewers_byteam.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[4]:
+# In[1]:
 
 
 import os
@@ -10,11 +10,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# In[35]:
+# In[5]:
 
 
 #import domestic league pass views data
-lpv_d = pd.read_csv('New_Domestic_League_Pass_Viewership.csv')
+lpv_d = pd.read_csv('../dataset/provided-data/New_Domestic_League_Pass_Viewership.csv')
+
+#regular season
+lpv_d.Date = pd.to_datetime(lpv_d.Date)
+lpv_d = lpv_d[(lpv_d.Date >= '10/17/2017') & (lpv_d.Date <= '4/11/2018') & ~(lpv_d.Home.isin(["EST","WST","USA","WLD"]))]
+
 
 #games for each team, regular season
 lpv_d2 = pd.concat([lpv_d.assign(Team = lpv_d['Home'], Opp = lpv_d['Away']),
@@ -23,9 +28,6 @@ lpv_d2 = pd.concat([lpv_d.assign(Team = lpv_d['Home'], Opp = lpv_d['Away']),
 #at home?
 lpv_d2['At_Home'] = (lpv_d2.Team == lpv_d2.Home)
 
-#regular season
-lpv_d2.Date = pd.to_datetime(lpv_d2.Date)
-lpv_d2 = lpv_d2[(lpv_d2.Date >= '10/17/2017') & (lpv_d2.Date <= '4/11/2018') & ~(lpv_d2.Team.isin(["EST","WST","USA","WLD"]))]
-
-#lpv_d2.to_csv('lpv_d2.csv')
+lpv_d.to_csv('../dataset/lpv_d.csv')
+lpv_d2.to_csv../dataset/lpv_d2.csv')
 
