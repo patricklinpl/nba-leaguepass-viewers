723a5e5c0a9f2f35e8b879c4cafc819e33ba36a9
Patrick Lin <patrick@patricklin.ca>
2018-09-16 11:56:02 -0400

    update nat-rating-date

--- dataset-merge-scripts/init.py
+++ dataset-merge-scripts/init.py
@@ -22,14 +22,20 @@ def merge_dma():
         base_CSV.at[i,'dma_home'] = dma.loc[dma['Team'] == row['Home']]['DMA_TV_Households_Millions'].item()
         base_CSV.at[i,'dma_away'] = dma.loc[dma['Team'] == row['Away']]['DMA_TV_Households_Millions'].item()
     base_CSV.to_csv('../dataset/merge-data/att-dma.csv', index=False) 
-        
+
+def merge_national_ratings():
+    base_CSV = pd.read_csv('../dataset/merge-data/att-dma.csv')
+    nat_rating = pd.read_csv('../dataset/provided-data/National_Ratings_by_Game.csv') 
+    # for i, row in nat_rating.iterrows():
+    #     nat_rating.at[i, 'Date'] = datetime.datetime.strptime(row['Date'], '%m/%d/%Y').date()
+    nat_rating.to_csv('../dataset/provided-data/National_Ratings_by_Game.csv', index=False)
 
 
 def merge_data_set():
     """This function matches and merges CSV to make one data set"""
     # merge_attendance()
-    merge_dma()
-    # merge_national_ratings()
+    # merge_dma()
+    merge_national_ratings()
 
 
 merge_data_set()
