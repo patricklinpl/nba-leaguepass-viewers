064aecde4b94e78668a643787a285d255110a8d0
Patrick Lin <patrick@patricklin.ca>
2018-09-16 11:09:27 -0400

    attendance-merge

---
+++ dataset-merge-scripts/init.py
@@ -0,0 +1,21 @@
+#!/usr/bin python3
+
+import pandas as pd
+import numpy as np
+import datetime as datetime
+
+def merge_attendance():
+    base_CSV = pd.read_csv('../dataset/lpv_d.csv')
+    attendance = pd.read_csv('../dataset/provided-data/Attendance_Capacity_Primary_Tickets.csv')
+    # for i, row in attendance.iterrows():
+    #     attendance.at[i, 'Date'] = datetime.datetime.strptime(row['Date'], '%m/%d/%Y').date()
+    # attendance.to_csv('../dataset/provided-data/Attendance_Capacity_Primary_Tickets.csv', index=False)
+    merged_df = pd.merge(base_CSV, attendance, how='outer', on=['Date', 'Home', 'Away'])
+    merged_df.to_csv('../dataset/merge-data/attendance-merge.csv', index=False)
+
+def merge_data_set():
+    """This function matches and merges CSV to make one data set"""
+    merge_attendance()
+
+
+merge_data_set()
